51249cffaf03e797309440cd231d2dee29769c2b
Annaliskirwa <annaliskirwa@gmail.com>
2022-02-27 16:08:53 +0300

    Added a log in to the application

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Auth0Provider} from "@auth0/auth0-react";
+
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Auth0Provider
+  domain="dev-zuzrkzsf.us.auth0.com"
+  clientId="orb3gGvMyMOK3weaaKF6q6MnQFo13aMH"
+  redirectUri={window.location.origin}
+>
+  <App />
+</Auth0Provider>,
   document.getElementById('root')
 );
